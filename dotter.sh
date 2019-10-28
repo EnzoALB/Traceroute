a054b71542ad351b9fdc25367de7011ebef0a78c
Hanzo <hanzo@enzo-albouy.local>
2019-10-28 12:12:10 +0100

    Script terminé + exemples

--- dotter.sh
+++ dotter.sh
@@ -2,30 +2,38 @@
 
 old_IFS=$IFS # sauvegarde du séparateur de champ
 IFS=$'\n' # nouveau séparateur de champ, le caractère fin de ligne
+prefixRTE=$1
 fichierdot=$2
+var=""
 
-sed -i '$ s/.$//' $fichierdot >> $fichierdot
-
-echo "Initialisation du fichier .dot"
-if [ ! "$(head -n2 $fichierdot |cut -d" " -f1 |grep "strict" )" ]
-  then echo -e "strict digraph essai1 { \nlocalhost [shape=house]; \n\"localhost\"" >> $fichierdot
-  else
-    echo -e "\nlocalhost [shape=house]; \n\"localhost\" " >> $fichierdot
-fi
-
-for ligne in $(cat $1)
+echo > $fichierdot
+for i in $(ls *.$prefixRTE.route)
 do
-  if echo $ligne |grep "Réseau"
-    then
-      echo -n -e " -> \"$ligne\" \n\"$ligne\" [shape=box color=red]; \n\"$ligne\"" >> $fichierdot
-    elif echo $ligne |grep "www."
-      then 
-        echo -n -e " -> \"$ligne\" \n\"$ligne\" [shape=house] " >> $fichierdot
-      else
-        echo " -> \"$ligne\"" >> $fichierdot
+  color=$(echo $RANDOM|md5sum|cut -c 1-6)
+  echo "Initialisation du fichier $i"
+  if [ ! "$(head -n2 $fichierdot |cut -d" " -f1 |grep "digraph" )" ]
+    then echo -e "digraph essai1 { \nlocalhost [shape=house]; \n\"localhost\"" >> $fichierdot
+    else
+      echo -e -n "\"localhost\"\n" >> $fichierdot
   fi
+
+  for ligne in $(cat $i)
+  do
+    if echo $ligne |grep "Réseau"
+      then
+        echo -n -e " -> \"$ligne\"\n" >> $fichierdot
+        var="$var \"$ligne\" [shape=box color=red];\n"
+      elif echo $ligne |grep "(.*)"
+        then 
+          echo -n -e " -> \"$ligne\" [color=\"#$color\"]\n" >> $fichierdot
+       	  var="$var \"$ligne\" [shape=house];\n"
+        else
+          echo " -> \"$ligne\"" >> $fichierdot
+    fi
+  done
 done
 
+echo -n -e $var >> $fichierdot
 echo "}" >> $fichierdot
 
 IFS=$old_IFS # rétablissement du séparateur de champ par défaut
